f8c19b3682bb8a3f3183536532fdd6188db12eb0
damien-martinez-hub <darkstoll97414@gmail.com>
2025-05-06 18:57:21 +0400

    test

---
+++ Documents/Private/Master1/irg/config.ml
@@ -0,0 +1,7 @@
+let install_dir = "/usr"
+let source_dir = "/mnt/c/Users/Utilisateur/gliss2/irg/.."
+let version = "2.3"
+let lowercase = String.lowercase
+let uppercase = String.uppercase
+let char_lowercase = Char.lowercase
+let char_uppercase = Char.uppercase
